0873dbf5c4ba743fb35a5eaea6926addab616d4e
Jordan Morris <jthecybertinkerer@gmail.com>
2018-12-08 23:04:26 -0600

    added main program files

---
+++ logs_analysis.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# logs_analysis.py
+# this file to analyze the html logs
+
+# task:
+#
+# On which days did more than 1% of requests lead to errors?
+# The log table includes a column status that indicates the HTTP status code that the news site sent to the user's browser.
