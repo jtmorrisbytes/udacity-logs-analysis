ef87b0e0c10f1d49a36f3f657ea427f3148c2baf
Jordan T Morris <jthecybertinkerer@gmail.com>
2018-12-16 13:38:15 -0600

    added a rudementary file writing function

--- logs_analysis.py
+++ logs_analysis.py
@@ -55,35 +55,60 @@
 
 # begin python code
 import os
+import sys
 import psycopg2
 
 
-error_analysis_query = "select to_char(date, 'J'), 100.0 * errors/total as percentage from  \
+error_analysis_query = "select date || U&'\\2014' || ' ' || to_char(100.0 * errors/total, '0.00') || '% errors' as percentage from  \
                             (select count(status) as total,                                      \
                                 count(case when status !='200 OK' then status end) as errors,    \
                                 to_char(time::date, 'Mon,DD YYYY') as date from public.log group by time::date)          \
                             as errorSummary where (100.0 * errors/total) > 1.0 order by date asc"
 
 popular_articles_query = \
-    "select '\"' || articles.title || '\" ' || U&'\\2014' || ' ' || trim(to_char(count(articles.title), '999999')) || ' views' from log \
+    "select '\"' || articles.title || '\" ' || U&'\\2014' || ' ' || trim(to_char(count(articles.title), '999,999')) || ' views' from log \
     full join articles on substring(path, character_length('/articles')+1, character_length(path)) = articles.slug \
     where log.path!='/' and log.status !='404 NOT FOUND' \
     group by articles.title order by count(articles.title) desc limit 3 "
 
-popular_authors_query = "select authorname, count(authorname) from(select authors.name as authorname, slug from articles \
-full join authors on author = authors.id) as authoredarticles                                                            \
-right join log on substring(log.path, character_length('/articles')+1, character_length(log.path)) = slug                \
-where log.path !='/' and log.status != '404 NOT FOUND' group by authorname order by count(authorname) desc"
+popular_authors_query = \
+    "select authorname || ' ' ||  U&'\\2014' || ' ' || trim(to_char(count(authorname), '9,999,999'))|| ' views' \
+    from    (select authors.name as authorname, slug from articles \
+                full join authors on author = authors.id \
+            ) as authoredarticles                                                            \
+            right join log on substring(log.path, character_length('/articles')+1, character_length(log.path)) = slug   \
+            where log.path !='/' and log.status != '404 NOT FOUND' group by authorname order by count(authorname) desc"
 analysis_log = []
 
 def results_to_array(cursor):
     query_result = cursor.fetchone()
     temp_array = []
     while query_result:
-        temp_array.append(query_result[0])
+        if len(query_result) == 1:
+            query_result = query_result[0]
+        temp_array.append(query_result)
         query_result = cursor.fetchone()
     
     return temp_array
+
+def write_log_file(input_array):
+    log_file_string = ""
+    for analysis_item_index in range(0, len(input_array)):
+        analasis_item = input_array[analysis_item_index]
+        title = analasis_item[0]
+        content = analasis_item[1]
+        log_file_string += "\t\t{}\n\n".format(title,)
+        for item_index in range(0,len(content)):
+            item = content[item_index]
+            item_string = ""
+            if analysis_item_index < 2:
+                item_string = "\t{}. {}\n".format(item_index+1, item,)
+            else:
+                item_string = "\t*  {}\n".format(item,)
+            log_file_string += "\t{}. {}\n".format(item_index+1, item,)
+        #print(content)
+    print(log_file_string)
+
 
 def do_collection(dbname, host, user, password, port = ''):
     try:
@@ -98,9 +123,14 @@ def do_collection(dbname, host, user, password, port = ''):
         
 
         analysis_log.append(("Popular Articles", results_to_array(cursor)))
-        print(analysis_log)
+        connection.commit()
+        cursor.execute(popular_authors_query)
+        analysis_log.append(("Popular Authors", results_to_array(cursor)))
+        connection.commit()
+        cursor.execute(error_analysis_query)
+        analysis_log.append(("Error Analasis Query", results_to_array(cursor)))
         connection.close()
-            
+        write_log_file(analysis_log)   
         
     except psycopg2.Error as error:
         print(error)
@@ -109,6 +139,14 @@ def do_collection(dbname, host, user, password, port = ''):
 
 
 if __name__ == "__main__":
+    arguments = sys.argv
+    print("arguments", arguments)
+    
+    for argument_index in range(1, len(arguments),):
+        argument = arguments[argument_index]
+        parameter = arguments[argument_index +1] 
+        if argument == '--outfile':
+
     dbname = "news"
     host = "sql-node1.jtmorrisbytes.com"
     user = "web"
